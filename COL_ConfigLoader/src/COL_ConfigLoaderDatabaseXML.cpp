e723d80c0d363bd5d17f02859ecdfcf405749b4e
1151682170@qq.com <1151682170@qq.com>
2024-03-26 20:01:57 +0800

    一些修改

--- COL_ConfigLoader/src/COL_ConfigLoaderDatabaseXML.cpp
+++ COL_ConfigLoader/src/COL_ConfigLoaderDatabaseXML.cpp
@@ -25,32 +25,62 @@ namespace HiddenButNotExposed {
         ConfigLoaderDatabaseXML::ErrorInfo 
             ConfigLoaderDatabaseXML::addConfig(const String& name, const String& value)
         {
-
-            return ErrorInfo();
+            if (!m_operatorDoc)return ErrorInfo::ERROR;
+            for (auto tool : m_operatorDoc->child("ConfigLoaderString").children()) {
+                if (tool.first_attribute().value() == name)return ErrorInfo::ADD_ERROR_NAME;
+            }
+            pugi::xml_node node = m_operatorDoc->child("ConfigLoaderString");
+            node.append_child("String");
+            node.last_child().append_attribute(name.c_str());
+            node.last_child().text().set(value.c_str());
+            return ErrorInfo::SUCCESS;
         }
 
         ConfigLoaderDatabaseXML::ErrorInfo 
             ConfigLoaderDatabaseXML::deleteConfig(const String& name, const String& value)
         {
-            return ErrorInfo();
+            if (!m_operatorDoc)return ErrorInfo::ERROR;
+            for (auto& tool : m_operatorDoc->child("ConfigLoaderString").children()) {
+                if (tool.first_attribute().value() == name) {
+                    if (tool.text().get() == value) {
+                        m_operatorDoc->child("ConfigLoaderString").remove_child(tool);
+                        return ErrorInfo::SUCCESS;
+                    }
+                    return ErrorInfo::DELETE_ERROR_VALUE;
+                }
+            }
+            return ErrorInfo::DELETE_ERROR_NAME;
         }
 
         ConfigLoaderDatabaseXML::ErrorInfo 
             ConfigLoaderDatabaseXML::changeConfig(const String& name, const String& value)
         {
-            return ErrorInfo();
+            if (!m_operatorDoc)return ErrorInfo::ERROR;
+            for (auto& tool : m_operatorDoc->child("ConfigLoaderString").children()) {
+                if (tool.first_attribute().value() == name) {
+                    tool.text().set(value.c_str());
+                    return ErrorInfo::SUCCESS;
+                }
+            }
+            return ErrorInfo::CHANGE_ERROR_NAME;
         }
 
         ConfigLoaderDatabaseXML::ErrorInfo 
             ConfigLoaderDatabaseXML::searchConfig(const String& name, String& value) const
         {
-            return ErrorInfo();
+            
         }
 
         ConfigLoaderDatabaseXML::ErrorInfo 
             ConfigLoaderDatabaseXML::getConfigMap(ConfigMap* map)
         {
-            return ErrorInfo();
+            if (map == NULL)map = new ConfigMap;
+            map->clear();
+            if (!m_operatorDoc)return ErrorInfo::ERROR;
+            for (auto tool : m_operatorDoc->child("ConfigLoaderString").children()) {
+                (*map)[tool.first_attribute().value()] = tool.text().get();
+            }
+            return ErrorInfo::SUCCESS;
         }
 
         ConfigLoaderDatabaseXML::ErrorInfo 
